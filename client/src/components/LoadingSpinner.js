1b9dbddd23416cc416317fcb01b63afa65eac825
Vuyisile <vtlehola23@gmail.com>
2022-11-21 17:00:22 +0200

    initial commit

---
+++ client/src/components/LoadingSpinner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Spinner from 'react-bootstrap/Spinner';
+
+const LoadingSpinner = () => {
+  return (
+    <div>
+       <Spinner animation="border" role="status" size="sm">
+         <span className="visually-hidden">Loading...</span>
+        </Spinner>
+    </div>
+  )
+}
+
+export default LoadingSpinner;
