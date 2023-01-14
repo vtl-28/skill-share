c0df8ce75606c20571287fb25aa97c7e345f3892
Vuyisile <vtlehola23@gmail.com>
2023-01-14 15:02:48 +0200

    initial commit

---
+++ client/src/components/miscellaneous/TalkLoading.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import { Skeleton } from 'react-ui';
+
+const TalkLoading = () => {
+    return(
+        <div className='flex flex-col'>
+            <Skeleton />
+            <Skeleton />
+            <Skeleton />
+            <Skeleton />
+            <Skeleton />
+            <Skeleton />
+            <Skeleton />
+            <Skeleton />
+            <Skeleton />
+        </div>
+    )
+}
+
+export default TalkLoading;
