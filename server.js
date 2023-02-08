e474bcd85e952ca227a2d1ae61802de22695d037
Vuyisile <vtlehola23@gmail.com>
2023-02-08 11:11:13 +0200

    setup socketio

--- server.js
+++ server.js
@@ -6,11 +6,57 @@ const talkRoutes = require("./routes/talkRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 const path = require("path");
 const cors = require("cors");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {  cors: {
+  origin: "http://localhost:3000",
+}});
 dotenv.config();
 connectDB();
+
+let onlineUsers = [];
+
+const addNewUser = (name, socketId) => {
+  !onlineUsers.some((user) => user.name === name) &&
+    onlineUsers.push({ name, socketId });
+    console.log(name + '' + 'added as new user')
+    console.log(onlineUsers)
+};
+
+const removeUser = (socketId) => {
+  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (name) => {
+  return onlineUsers.find((user) => user.name === name);
+};
+
+const addTalk = (user, talk) => {
+
+}
+
+io.on("connection", (socket) => {
+  console.log('user connected')
+  socket.on('newUser', (user) => {
+    let name = user ? user.name : ''
+    addNewUser(name, socket.id)
+  })
+  socket.on('sendNotification', ({senderName, type}) => {
+    socket.broadcast.emit('getNotification', {
+      senderName,
+      type
+    })
+  })
+
+  socket.on('disconnect', () => {
+    removeUser(socket.id)
+    console.log('user disconnected')
+  })
+});
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
@@ -29,7 +75,7 @@ app.use(errorHandler);
 
 
 
-app.listen(
+httpServer.listen(
   PORT,
   console.log(`Server running on PORT ${PORT}...`.yellow.bold)
 );
